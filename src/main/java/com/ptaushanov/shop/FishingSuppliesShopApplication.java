660931b3ec0005d4f35913ce529d25123a9b04fd
ptaushanov <pepi15032000@gmail.com>
2023-10-28 20:00:25 +0300

    feat: initial setup

---
+++ src/main/java/com/ptaushanov/shop/FishingSuppliesShopApplication.java
@@ -0,0 +1,13 @@
+package com.ptaushanov.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingSuppliesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingSuppliesShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptaushanov/shop/FishingSuppliesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptaushanov.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingSuppliesShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
