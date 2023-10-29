eb4a1db33d140cef144baed4d50e0488fe528ac0
ptaushanov <pepi15032000@gmail.com>
2023-10-29 13:49:13 +0200

    feat: added username extraction from jwt

--- src/main/java/com/ptaushanov/shop/security/JwtService.java
+++ src/main/java/com/ptaushanov/shop/security/JwtService.java
@@ -1,11 +1,42 @@
 package com.ptaushanov.shop.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    @Value("${jwt.secret-key}")
+    private static String secretKey;
+
     public String extractUsername(String jwt) {
-        return null;
+        return extractClaim(jwt, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String jwt, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwt) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] decodedKey = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(decodedKey);
+
     }
 }
