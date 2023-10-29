09ab660fded891d8f0c3b356102ce4e4b2f53ceb
ptaushanov <pepi15032000@gmail.com>
2023-10-29 13:05:22 +0200

    feat: added user repository

---
+++ src/main/java/com/ptaushanov/shop/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ptaushanov.shop.repository;
+
+import com.ptaushanov.shop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+}
