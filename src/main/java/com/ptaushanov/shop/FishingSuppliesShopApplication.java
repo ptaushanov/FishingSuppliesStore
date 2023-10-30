991c0a05bb757020089c585795bde0663b0f7842
ptaushanov <pepi15032000@gmail.com>
2023-10-30 21:55:20 +0200

    fix: changed mapping configuration

--- src/main/java/com/ptaushanov/shop/FishingSuppliesShopApplication.java
+++ src/main/java/com/ptaushanov/shop/FishingSuppliesShopApplication.java
@@ -2,12 +2,14 @@ package com.ptaushanov.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ptaushanov.shop.mapper") // Managing mappers as beans
 public class FishingSuppliesShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FishingSuppliesShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FishingSuppliesShopApplication.class, args);
+    }
 
 }
