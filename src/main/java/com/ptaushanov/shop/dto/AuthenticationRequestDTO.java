e2649bc1d6642cef4aec595d16c1e77ec53a839f
ptaushanov <pepi15032000@gmail.com>
2023-11-01 23:45:43 +0200

    feat: auth dto validation

--- src/main/java/com/ptaushanov/shop/dto/AuthenticationRequestDTO.java
+++ src/main/java/com/ptaushanov/shop/dto/AuthenticationRequestDTO.java
@@ -1,5 +1,6 @@
 package com.ptaushanov.shop.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequestDTO {
+    @NotNull(message = "Username cannot be null")
     private String username;
+
+    @NotNull(message = "Password cannot be null")
     private String password;
 }
