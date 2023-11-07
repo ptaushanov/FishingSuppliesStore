4b873180690616a089b2686d4923519c7297f82a
ptaushanov <pepi15032000@gmail.com>
2023-11-07 12:30:08 +0200

    feat: all users get mapping

--- src/main/java/com/ptaushanov/shop/controller/UserController.java
+++ src/main/java/com/ptaushanov/shop/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ptaushanov.shop.dto.user.UserRequestDTO;
 import com.ptaushanov.shop.dto.user.UserResponseDTO;
 import com.ptaushanov.shop.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping
+    public Page<UserResponseDTO> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id,asc") String sortString
+    ) {
+        return userService.getAllUsers(page, size, sortString);
+    }
 
     @GetMapping(path = "/{id}")
     public UserResponseDTO getUserById(@PathVariable Long id) {
