8bc6cbc01c4f0ef758df2689cf9491cdc3adde11
ptaushanov <pepi15032000@gmail.com>
2023-11-26 01:27:32 +0200

    feat: added FilterSpecification

---
+++ src/main/java/com/ptaushanov/shop/util/FilterSpecification.java
@@ -0,0 +1,52 @@
+package com.ptaushanov.shop.util;
+
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.jpa.domain.Specification;
+
+public class FilterSpecification {
+    private static final Logger logger = LoggerFactory.getLogger(FilterSpecification.class);
+    private static ConversionService conversionService;
+
+    public static <T> Specification<T> filterQuery(String filter) {
+        return (root, query, criteriaBuilder) -> {
+            if (filter == null || filter.isEmpty()) return criteriaBuilder.conjunction();
+            String[] filterParams = filter.split(",");
+            if (filterParams.length < 2) return criteriaBuilder.conjunction();
+
+            String filterProperty = filterParams[0];
+            String filterValue = filterParams[1];
+
+            EntityType<T> entityType = root.getModel();
+            SingularAttribute<? super T, ?> singularAttribute = entityType.getSingularAttribute(
+                    filterProperty);
+
+            Class<?> propertyType = singularAttribute.getJavaType();
+            Object convertedValue = convertToPropertyType(filterValue, propertyType);
+
+            return criteriaBuilder.equal(root.get(filterProperty), convertedValue);
+        };
+    }
+
+    private static Object convertToPropertyType(String filterValue, Class<?> propertyType) {
+        if (propertyType == String.class) return filterValue;
+        try {
+            return conversionService.convert(filterValue, propertyType);
+        } catch (Exception e) {
+            logger.error(
+                    "Error converting value '{}' to type '{}'", filterValue,
+                    propertyType.getSimpleName(), e
+            );
+            return null;
+        }
+    }
+
+    @Autowired
+    public void setConversionService(ConversionService conversionService) {
+        FilterSpecification.conversionService = conversionService;
+    }
+}
