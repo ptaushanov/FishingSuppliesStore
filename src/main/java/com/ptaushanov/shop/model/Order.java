7a7371528d5a52e9a9de8f43307bb113d4528893
ptaushanov <pepi15032000@gmail.com>
2023-11-26 21:35:13 +0200

    feat: added order model

---
+++ src/main/java/com/ptaushanov/shop/model/Order.java
@@ -0,0 +1,58 @@
+package com.ptaushanov.shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User customer;
+
+    @Column(nullable = false)
+    @ManyToMany
+    @JoinColumn(name = "product_id", nullable = false)
+    private List<Product> products;
+
+    @Column(nullable = false)
+    private double totalPrice;
+
+    public Order(User customer, List<Product> products) {
+        this.customer = customer;
+        this.products = products;
+    }
+
+    @PrePersist
+    @PreUpdate
+    private void calculateTotalPrice() {
+        if (products != null) {
+            double total = 0.0;
+            for (Product product : products) {
+                total += product.getPrice();
+            }
+            this.totalPrice = total;
+        }
+    }
+}
